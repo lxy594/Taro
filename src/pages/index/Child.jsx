dee4f5b114a2f1e3d5a840763b56ffde4e3fd4cc
liuxiaoyin <8253192+xy599@user.noreply.gitee.com>
2021-02-20 15:03:12 +0800

    创建组件以及导入

---
+++ src/pages/index/Child.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View,Text} from '@tarojs/components'
+function Child(){
+    return(
+        <View>
+            <Text>我是子组件Child.</Text>
+        </View>
+    )
+}
+export default Child;
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { View, Text } from '@tarojs/components'
+// 通过导入子组件
+import Child from './Child.jsx'
 import './index.less'
-
 function Index(){
 const [useName,setUsename]=useState('lxy.vcom')
 return(
   <View>
   <Text>{useName}</Text>
+  <Child></Child>
   </View>
 )
 }
