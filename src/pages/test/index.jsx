a7c91cf54ede2e478e8bd73b3ad8b6fe4b46139b
liuxiaoyin <8253192+xy599@user.noreply.gitee.com>
2021-02-20 21:31:45 +0800

    配置路由的页面

---
+++ src/pages/test/index.jsx
@@ -0,0 +1,15 @@
+import Taro from '@tarojs/taro'
+import { View, Text, Button } from '@tarojs/components'
+import React from 'react'
+function Test() {
+
+    return (
+        <View>
+            <Text>
+                新建的文件
+            </Text>
+           
+        </View>
+    )
+}
+export default Test;
