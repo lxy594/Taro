133ec70e78f25141341c0124d90c53076a8572ec
liuxiaoyin <8253192+xy599@user.noreply.gitee.com>
2021-02-22 19:43:34 +0800

    查询字符串的方式传参

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text } from '@tarojs/components'
 // 通过导入子组件
 import Child from './Child.jsx'
-import './index.less'
-function Index(){
-const [useName,setUsename]=useState('lxy.vcom')
-return(
-  <View>
-  <Text>{useName}</Text>
-  <Child userchild={useName}></Child>
-  </View>
-)
+import Taro, { Current } from '@tarojs/taro'
+
+function Index() {
+
+  const [transfer, setTransfer] = useState('')
+  const [useName] = useState('lxy.vcom')
+  
+  useEffect(() => {
+    // Current=Taro.getCurrentInstance
+    setTransfer(Current.router.params.transfer)
+  }, [])
+  return (
+    <View>
+      <Text>{useName}</Text>
+      <Child userchild={useName}></Child>
+      <View>{transfer}</View>
+    </View>
+  )
 }
 export default Index; 
--- src/pages/test/index.jsx
+++ src/pages/test/index.jsx
@@ -1,16 +1,11 @@
 import Taro from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
-import React from 'react'
-function Test() {
-    const gotoIndex = () => {
-        // 可以返回到上一级
-        Taro.navigateTo({ url: '/pages/index/index' })
-        
-        // 不可以返回到上一级
-        // Taro.reLaunch({url:'/pages/index/index'})
+import React, { useState } from 'react'
 
-        //返回上一页面或多级页面。可通过 getCurrentPages 获取当前的页面栈，决定需要返回几层。
-        //Taro.navigateBack({url:'/pages/index/index'})
+function Test() {
+    const [transfer, setTransfer] = useState('lxysss')
+    const gotoIndex = () => {
+        Taro.navigateTo({ url: '/pages/index/index?transfer=' + transfer })
     }
     return (
         <View>
